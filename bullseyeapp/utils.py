a4503a753673e0ec6c327705b115960a435b2a12
General Notability <generalnotability@gmail.com>
2021-08-23 17:48:41 -0400

    Add raise_for_status, rework data_sources

--- bullseyeapp/utils.py
+++ bullseyeapp/utils.py
@@ -3,5 +3,5 @@ import requests
 
 def get_whois_data(ip):
     r = requests.get(f'https://whois-dev.toolforge.org/w/{ip}/lookup/json')
-    print(r.text)
+    r.raise_for_status()
     return r.json()
--- bullseyeapp/views.py
+++ bullseyeapp/views.py
@@ -5,9 +5,15 @@ from .utils import get_whois_data
 def get_ip_info(request, ip):
     context = {}
     context['ip'] = ip
-    whois_data = get_whois_data(ip)
-    context['whois'] = whois_data
-    context['data_sources'] = {'whois': True}
+    context['data_sources'] = {}
+    try:
+        whois_data = get_whois_data(ip)
+        context['whois'] = whois_data
+        context['data_sources']['whois'] = True
+    # FIXME: bare except
+    except:
+        context['data_sources']['whois'] = False
+
     return render(request, 'bullseye/ip.html', context)
 
 def get_ip_range_info(request, ip, cidr):
