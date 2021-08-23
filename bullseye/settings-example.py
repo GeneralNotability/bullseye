aab34cfb7c1ff114d25e34232528b5e6ab54ad46
General Notability <generalnotability@gmail.com>
2021-08-23 18:04:24 -0400

    Integrate maxmind

--- bullseye/settings-example.py
+++ bullseye/settings-example.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GEOIP_PATH = '/c/Users/Chris/workdir/bullseye/geodb'
--- bullseyeapp/views.py
+++ bullseyeapp/views.py
@@ -1,19 +1,32 @@
+from django.conf import settings
+from django.contrib.gis.geoip2 import GeoIP2
 from django.http import HttpResponse
 from django.shortcuts import render
+from requests.exceptions import HTTPError
 
 from .utils import get_whois_data
 def get_ip_info(request, ip):
     context = {}
     context['ip'] = ip
     context['data_sources'] = {}
+
     try:
         whois_data = get_whois_data(ip)
         context['whois'] = whois_data
         context['data_sources']['whois'] = True
     # FIXME: bare except
-    except:
+    except HTTPError:
         context['data_sources']['whois'] = False
 
+    if hasattr(settings, 'GEOIP_PATH') and settings.GEOIP_PATH:
+        try:
+            g = GeoIP2()
+            context['maxmind'] = g.city(ip)
+            context['data_sources']['maxmind'] = True
+        except Exception as e:
+            print(e)
+            context['data_sources']['maxmind'] = False
+    
     return render(request, 'bullseye/ip.html', context)
 
 def get_ip_range_info(request, ip, cidr):
