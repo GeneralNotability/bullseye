9efbe3db5ce12e5813086ead1023fb6b14369963
General Notability <generalnotability@gmail.com>
2021-08-23 14:30:29 -0400

    Initial template view

--- bullseyeapp/apps.py
+++ bullseyeapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BullseyeappConfig(AppConfig):
+class BullseyeAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bullseyeapp'
--- bullseyeapp/views.py
+++ bullseyeapp/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_ip_info(request, ip):
-    return HttpResponse(f'Details of {ip}')
+    context = {}
+    context['ip'] = ip
+    return render(request, 'bullseye/ip.html', context)
 
 def get_ip_range_info(request, ip, cidr):
     return HttpResponse(f'Details of {ip}/{cidr}')
