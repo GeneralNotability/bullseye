25d27120d3ba36c6217cfa21547363f92f1c2754
General Notability <generalnotability@gmail.com>
2021-08-23 16:35:37 -0400

    Rework template setup, modularize

--- bullseyeapp/views.py
+++ bullseyeapp/views.py
@@ -6,9 +6,8 @@ def get_ip_info(request, ip):
     context = {}
     context['ip'] = ip
     whois_data = get_whois_data(ip)
-    context['whois'] = {}
-    context['whois']['asn'] = whois_data.get('asn', 'UNKNOWN')
-
+    context['whois'] = whois_data
+    context['data_sources'] = {'whois': True}
     return render(request, 'bullseye/ip.html', context)
 
 def get_ip_range_info(request, ip, cidr):
