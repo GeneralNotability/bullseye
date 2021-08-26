78bd742ab490699402fa478d7ff31d46fa8454e8
General Notability <generalnotability@gmail.com>
2021-08-26 10:40:46 -0400

    Make cache object

--- bullseyeapp/models.py
+++ bullseyeapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class CachedResult(models.Model):
+
+    ip_addr = models.GenericIPAddressField()
+    source = models.CharField(max_length=30)
+    updated = models.DateField(auto_now=True)
+    result = models.TextField()
+
+    unique_ip_source = models.UniqueConstraint(name='unique_ip_source',
+                                               fields=['ip_addr', 'source'])
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['ip_addr', 'source'])
+        ]
