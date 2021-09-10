6f69b02b169849df6ed8ce1f79e2e4158310d7c8
General Notability <generalnotability@gmail.com>
2021-09-10 14:56:00 -0400

    Add tools bar

--- bullseyeapp/urls.py
+++ bullseyeapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_landing_page),
+    path('', views.get_landing_page, name='landing_page'),
     path('logout', views.logout_view, name='logout'),
     path('ip/<str:ip>', views.get_ip_info, name='ip'),
     path('ip/<str:ip>/<int:cidr>', views.get_ip_range_info)
